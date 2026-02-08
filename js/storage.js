a4b5dce95a8a0c4fd0d6f7acd0ed0c26699f0243
Mostafa <mostafaelsehy1@gmail.com>
2026-02-08 22:03:55 +0200

    added storage.js logic

---
+++ js/storage.js
@@ -0,0 +1,24 @@
+function saveToLocal(key, data) {
+  try {
+    const jsonData = JSON.stringify(data); // تحويل البيانات إلى JSON String
+    localStorage.setItem(key, jsonData); // التخزين بالمفتاح
+  } catch (error) {
+    console.error("Storage Full! Image too large.");
+  }
+}
+
+function getFromLocal(key) {
+  const storedData = localStorage.getItem(key);
+
+  // لو مفيش بيانات
+  if (!storedData) {
+    return null;
+  }
+
+  try {
+    return JSON.parse(storedData); // تحويل من JSON إلى Object
+  } catch (error) {
+    console.error("Invalid data for this key.");
+    return null;
+  }
+}
